0182f720afffce9a45c6262da0d63555f5289905
Jakub Urban <urbanjakub1306@gmail.com>
2022-01-05 13:50:13 +0100

    Array search improvement

--- tests/array-search.js
+++ tests/array-search.js
@@ -3,22 +3,23 @@
 const Meter = require('performancemeter');
 
 Meter.measure('indexOf', function() {
-	arr.indexOf(3) !== -1;
+	var includes = arr.indexOf(3) !== -1;
 }, function() {
 	var arr = [1,2,3,4,5];
 });
 
 Meter.measure('for', function() {
+	var result = false;
 	for (var i = 0; i < arr.length; i++) {
 		if (arr[i] === 3)
-			break;
+			result = true;
 	}
 }, function() {
 	var arr = [1,2,3,4,5];
 });
 
 Meter.measure('includes', function() {
-	arr.includes(3);
+	var result = arr.includes(3);
 }, function() {
 	var arr = [1,2,3,4,5];
 });
